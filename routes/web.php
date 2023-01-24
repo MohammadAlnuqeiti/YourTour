a6a4ab0cb3993a038d42dbeef5668a56aeac9581
malnuqeiti-988 <mohammadalnuqeiti@gmail.com>
2023-01-24 19:26:29 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/admin', function () {
+    return view('admin.welcome');
+});
+Route::get('/admin/table', function () {
+    return view('admin.table');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
+
